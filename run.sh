b346e2bcfcc02bdcd71d816fcc2f65a7ea2d3515
mksmki <mksmki@example.com>
2023-08-15 04:21:28 +0000

    Add test PB to split long lists

--- run.sh
+++ run.sh
@@ -2,10 +2,12 @@
 
 set +x
 
-python3 -m venv .ansible
-source ./.ansible/bin/activate
+python3 -m venv .venv
+source ./.venv/bin/activate
 
-pip install ansible
+pip install \
+    ansible \
+    ansible-lint
 
 time ansible-playbook -i localhost, main.yaml
 time ansible-playbook -i localhost, parallel_ping.yaml
